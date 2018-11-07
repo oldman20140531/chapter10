957a4fe8e0c5dbdf3e47c89d4b08b10eda3adcd5
L <lsj2521398@gmail.com>
2018-11-07 16:42:04 +0800

    test

--- FindEngWord.py
+++ FindEngWord.py
@@ -6,3 +6,4 @@ with open('from.txt', encoding='utf8') as f:
 word.sort()                                         # 排序
 with open('to.txt', 'w', encoding='utf8') as f:
     f.write('\n'.join(i for i in word))             # 将word列表重组为一个字符串并加入换行符后写入新文件
+
